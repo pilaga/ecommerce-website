11b5bef03e125f9dad8585bfe43ef6e13e75eafd
Pierre <pierre.lagadec@gmail.com>
2021-11-17 10:54:12 +1100

    serving css files statically

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 /*app.use('/users', (req, res, next) => {
