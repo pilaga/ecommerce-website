6f0b13178ac40f462b54256bdf982dd79c5ccc44
Pierre <pierre.lagadec@gmail.com>
2021-11-16 17:58:10 +1100

    adding 404 page

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.use(express.urlencoded({extended: false}));
 app.use(adminRouter);
 app.use(shopRouter);
 
+app.use((req, res, next) => {
+    res.status(404)
+    .send('<h2>Page not found</h2>');
+});
 
 app.listen(3000);
 
