0e6fa4a079e5db6c06d82c1a12c14f5753884ce8
Pierre <pierre.lagadec@gmail.com>
2021-11-16 17:41:57 +1100

    parsing incoming requests

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.use('/', (req, res, next) => {
-    console.log('this always runs');
+    //console.log('this always runs');
     next();
 });
 
+app.use('/users', (req, res, next) => {
+    res.send('<h2>users page</h2>');
+});
+
 app.use('/add-product', (req, res, next) => {
-    console.log('adding product middleware');
-    res.send('<h2>add-product page</h2>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+})
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body.title);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => { 
-    console.log('in another middleware');
     res.send('<h2>Hello from express</h2>');
 });
 
