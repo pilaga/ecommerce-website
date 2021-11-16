8b0edff1b94dedaf0c03b95155ffa6156fd5c648
Pierre <pierre.lagadec@gmail.com>
2021-11-16 13:34:40 +1100

    isntalling Express.js and adding middleware

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const http = require('http');
+const express = require('express');
 
+const app = express();
 
+app.use((req, res, next) => {
+    console.log('in the middleware');
+    next();
+});
 
-console.log(routes.someText);
+app.use((req, res, next) => {
+    console.log('in another middleware');
+});
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(3000);
