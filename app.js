5c8d3d157571f8646f5729b1e7f36fa4dda54e72
Pierre <pierre.lagadec@gmail.com>
2021-11-16 17:53:38 +1100

    using Express router

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.use('/', (req, res, next) => {
-    //console.log('this always runs');
-    next();
-});
 
-app.use('/users', (req, res, next) => {
+/*app.use('/users', (req, res, next) => {
     res.send('<h2>users page</h2>');
-});
+});*/
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
-})
+app.use(adminRouter);
+app.use(shopRouter);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body.title);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => { 
-    res.send('<h2>Hello from express</h2>');
-});
 
 app.listen(3000);
 
