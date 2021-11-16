e00c628815c1b0ee6894bfde2c9f5eb588433aa3
Pierre <pierre.lagadec@gmail.com>
2021-11-17 09:52:17 +1100

    added basic styling to our pages

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const rootDir = require('./utils/path_helper');
+
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
@@ -20,7 +22,7 @@ app.use(shopRouter);
 
 app.use((req, res, next) => {
     res.status(404)
-    .sendFile(path.join(__dirname, 'views', '404.html'));
+    .sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,11 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+const rootDir = require('../utils/path_helper');
+
 //admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
 
 //admin/add-product => POST
