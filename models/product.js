9506a7bc92932cf19076fcf99e75de501a99f163
Pierre <pierre.lagadec@gmail.com>
2021-11-18 12:54:56 +1100

    MVC - storing data in a file via model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,6 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../utils/path_helper');
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +8,33 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        const filepath = path.join(rootDir, 'data', 'products.json');
+
+        //read file first
+        fs.readFile(filepath, (err, data) => { 
+            let products = [];   
+            //if file exists, read existing products from file       
+            if(!err) {   
+                products = JSON.parse(data);
+            }
+            //add new product
+            products.push(this);             
+            //write file again
+            fs.writeFile(filepath, JSON.stringify(products), (err) => {
+                console.log("Error:", err);
+            });
+        });
     }
 
     static fetchAll() {
-        return products;
+        const filepath = path.join(rootDir, 'data', 'products.json');
+
+        //retrieve and parse file content
+        fs.readFile(filepath, (err, data) => {
+            if(err) {
+                return [];
+            }
+            return JSON.parse(data);
+        });
     }
 }
