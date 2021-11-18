ef7800160e18ca04d43111208f9633b6d809601a
Pierre <pierre.lagadec@gmail.com>
2021-11-18 14:06:02 +1100

    improving the shop structure, updating navigation

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', 
+    res.render('./admin/add-product', 
         { 
             pagetitle: "Add Product", 
             path: '/admin/add-product',
@@ -19,7 +19,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => { 
     const products = Product.fetchAll((products) => {
-        res.render('shop', 
+        res.render('./shop/product-list', 
             { 
                 products: products,
                 pagetitle: 'My Shop', 
