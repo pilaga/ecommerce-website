a71bfa129fb4fbdea4ef21db79db40ecae036e7e
Pierre <pierre.lagadec@gmail.com>
2021-11-29 14:02:40 +1100

    mongoose - creating product schema

--- models/product.js
+++ models/product.js
@@ -1,3 +1,26 @@
+const { Double } = require('bson');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+});
+
 // const mongodb = require('mongodb');
 // const mongo = require('../utils/database');
 
