a3b5af857a2c5c8840168929cbfcac627eef7a3d
Pierre <pierre.lagadec@gmail.com>
2021-12-07 12:30:51 +1100

    retaining user input after validation fail

--- controllers/auth.js
+++ controllers/auth.js
@@ -78,13 +78,19 @@ exports.getSignup = (req, res, next) => {
     { 
         pagetitle: "Signup",
         path: "/signup",
-        errorMessage: req.flash('error')        
+        errorMessage: req.flash('error'),
+        userInput: {
+            email: "",
+            password: "",
+            confirmPassword: ""
+        }    
     });  
 }
 
 exports.postLogin = (req, res, next) => {    
     const email = req.body.email;
     const password = req.body.password;
+
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(422).render('./auth/login', 
@@ -94,6 +100,7 @@ exports.postLogin = (req, res, next) => {
             errorMessage: errors.array()[0].msg  
         });    
     }
+
     User.findOne({ email: email })
     .then(user => {
         if(!user) {
@@ -168,15 +175,22 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(422).render('./auth/signup', 
         { 
             pagetitle: "Signup",
             path: "/signup",
-            errorMessage: errors.array()[0].msg       
+            errorMessage: errors.array()[0].msg,
+            userInput: {
+                email: email,
+                password: password,
+                confirmPassword: req.body.confirmPassword
+            }   
         });  
     }
+
     //here - check user input
     //check email doesn't exit
     bcrypt.hash(password, 12)
