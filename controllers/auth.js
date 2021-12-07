c6465ce58b83f0c110f322e4e5989e3fc6004596
Pierre <pierre.lagadec@gmail.com>
2021-12-07 12:40:50 +1100

    adding conditional CSS validation/error formatting

--- controllers/auth.js
+++ controllers/auth.js
@@ -83,7 +83,8 @@ exports.getSignup = (req, res, next) => {
             email: "",
             password: "",
             confirmPassword: ""
-        }    
+        },
+        validationErrors: [] 
     });  
 }
 
@@ -187,7 +188,8 @@ exports.postSignup = (req, res, next) => {
                 email: email,
                 password: password,
                 confirmPassword: req.body.confirmPassword
-            }   
+            },
+            validationErrors: errors.array()
         });  
     }
 
