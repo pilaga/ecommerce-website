9a8babc55920d01340c439e6c40953e9237a25db
Pierre <pierre.lagadec@gmail.com>
2021-12-07 11:36:46 +1100

    checking for field equality (passwords)

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -10,14 +10,27 @@ router.get('/reset', authController.getReset);
 router.get('/reset/:token', authController.getNewPassword)
 router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
-router.post('/signup', 
-    check('email').isEmail().withMessage('Email address is invalid')
-    .custom((value, {req}) => {
-        if(value === "test@test.com"){
-            throw new Error ('This email address is forbidden');
-        }
-        return true;
-    }), 
+router.post('/signup',
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Email address is invalid')
+            .custom((value, {req}) => {
+                if(value === "test@test.com"){
+                    throw new Error ('This email address is forbidden');
+                }
+                return true;
+            }),
+        body('password', 'Please enter a password with number/text and at least 5 characters')
+            .isLength({ min: 5 })        
+            .isAlphanumeric(),
+        body('confirmPassword').custom((value, { req }) => {
+            if(value !== req.body.password) {
+                throw new Error('Passwords do not match!');
+            }
+            return true;
+        })
+    ],
     authController.postSignup);
 router.post('/reset', authController.postReset);
 router.post('/new-password', authController.postNewPassword);
