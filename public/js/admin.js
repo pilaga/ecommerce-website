2ef2769a8841284792fdf9594f2fd3dd1d8be6d9
Pierre <pierre.lagadec@gmail.com>
2021-12-17 14:09:01 +1100

    manipulating the DOM to remove product

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,7 +1,7 @@
 const deleteProduct = (button) => {
     const prodId = button.parentNode.querySelector('[name=productId]').value;
     const csrf = button.parentNode.querySelector('[name=_csrf]').value;
-    //console.log(prodId);
+    const productElement = button.closest('article');
     fetch('/admin/product/' + prodId, {
         method: 'DELETE',
         headers: {
@@ -9,7 +9,12 @@ const deleteProduct = (button) => {
         }
     })
     .then(result => {
-        console.log(result);
+        return result.json();
+    })
+    .then(data => {
+        console.log(data);
+        //productElement.remove();
+        productElement.parentNode.removeChild(productElement);
     })
     .catch(err => console.log(err));
 };
